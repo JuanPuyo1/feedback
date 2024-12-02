0d5ec94f7941f87ce36ae5a7fbad93976b7b2977
Esteban Puyo <jpuyoc@ucentral.edu.co>
2024-12-01 19:35:56 -0800

    changing to the model form approach

--- reviews/models.py
+++ reviews/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class Review(models.Model):
     user_name = models.CharField(max_length=100)
-    review_text = models.CharField(max_length=200)
-    rating = models.IntegerField(min_value=1,max_value=5)
+    review_text = models.TextField()
+    rating = models.IntegerField()
